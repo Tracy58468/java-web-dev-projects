6e96be5d302778c92048e523a23575dcd09b3e99
Tracy H <aag9n17o@duck.com>
2023-08-16 21:03:14 -0500

    Trying to commit again.

--- datatypes/datatypes-exercises/src/main/java/org/launchcode/MilesPerGallon.java
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/MilesPerGallon.java
@@ -5,10 +5,10 @@ public class MilesPerGallon {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("How many miles have you driven?  ");
-        double milesDriven = input.nextDouble();
+        Double milesDriven = input.nextDouble();
         System.out.println("How many gallons of gas have you used driving those miles?  ");
-        double gasUsed = input.nextDouble();
-        double milesPerGallon = milesDriven / gasUsed;
+        Double gasUsed = input.nextDouble();
+        Double milesPerGallon = milesDriven / gasUsed;
         System.out.println("Your gas mileage is " + milesPerGallon + " mpg.");
     }
 }
--- datatypes/datatypes-exercises/src/main/java/org/launchcode/RectangleArea.java
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/RectangleArea.java
@@ -1,2 +1,14 @@
-package org.launchcode;public class RectangleArea {
+package org.launchcode;
+
+import java.util.Scanner;
+public class RectangleArea {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the length of the short side  ");
+        Double shortSide = input.nextDouble();
+        System.out.println("What is the length of the long side  ");
+        Double longSide = input.nextDouble();
+        Double area = shortSide * longSide;
+        System.out.println("The area of your rectangle is " + area);
+    }
 }
