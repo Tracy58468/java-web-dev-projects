740e5b5715457f5eb28d42d4377c378fd65c0845
Tracy H <aag9n17o@duck.com>
2023-08-28 20:09:24 -0500

    Add import statement for ArrayList

--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -1,11 +1,13 @@
 package org.launchcode;
 
+import java.util.ArrayList;
+
 public class Menu {
     ArrayList<MenuItem> menuItems = new ArrayList<> {};
 
     public void addMenuItem(MenuItem menuItem) {
         menuItems.add(menuItem);
 
-        // update isNew on old items (x days before new item add date) when item is added - use Date class
+        // update isNew on old items (x days before new item add date) when item is added - use Date class menuItem.isNew = false;
     }
 }
