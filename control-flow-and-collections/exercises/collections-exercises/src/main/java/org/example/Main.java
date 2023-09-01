60bded6355a2e2f90c7a69991ddadecb794945ac
Tracy H <aag9n17o@duck.com>
2023-08-31 19:57:47 -0500

    Finished exercises for chapter 3.

--- control-flow-and-collections/exercises/collections-exercises/src/main/java/org/example/Main.java
+++ control-flow-and-collections/exercises/collections-exercises/src/main/java/org/example/Main.java
@@ -1,19 +1,91 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        int[] integerArray = {1, 1, 2, 3, 5, 8};
+        String eggsAndHam = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        String[] eggsAndHamStringArray = eggsAndHam.split("\\. ");
+
+        System.out.println(Arrays.toString(eggsAndHamStringArray));
+
+        for (int i : integerArray) {
+
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+
         }
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        numbers.add(10);
+        numbers.add(7);
+        numbers.add(2);
+        numbers.add(5);
+        numbers.add(8);
+        numbers.add(30);
+        numbers.add(13);
+        numbers.add(21);
+        numbers.add(15);
+        numbers.add(2);
+
+        System.out.println(sumEven(numbers));
+
+        ArrayList<String> words = new ArrayList<>();
+
+        words.add("time");
+        words.add("timer");
+        words.add("times");
+        words.add("timetable");
+        words.add("tim");
+
+//        System.out.println(words);
+        System.out.println(printWords(words));
+
     }
+
+    public static int sumEven(ArrayList<Integer> numbers) {
+
+        int j = 0;
+
+        for (int k : numbers) {
+            if (k % 2 == 0) {
+                j = j + k;
+            }
+
+        }
+
+        return j;
+
+    }
+
+    public static ArrayList printWords(ArrayList<String> words) {
+
+        ArrayList<String> pickedWords = new ArrayList<>();
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the number of letters you would like to search for. ");
+
+        int numChars = input.nextInt();
+
+        for (String m : words) {
+            if (m.length() == numChars) {
+                pickedWords.add(m);
+//                System.out.println("pickedWords end " + pickedWords);
+            }
+        }
+
+        return pickedWords;
+    }
+
 }
